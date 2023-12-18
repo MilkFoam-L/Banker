524155661187bf5a518f60ebdf2f2ce177b26303
Neige <2468629609@qq.com>
2023-12-18 21:01:55 +0800

    v1.2.5 update
    banker reload异步调度中传入Banker插件实例，防止报错时显示由NeigeItems导致

--- src/main/kotlin/pers/neige/banker/command/subcommand/Reload.kt
+++ src/main/kotlin/pers/neige/banker/command/subcommand/Reload.kt
@@ -1,12 +1,13 @@
 package pers.neige.banker.command.subcommand
 
 import org.bukkit.command.CommandSender
+import pers.neige.banker.Banker
 import pers.neige.banker.manager.ConfigManager
 import pers.neige.neigeitems.utils.SchedulerUtils.async
 
 object Reload {
     fun reloadCommand(sender: CommandSender) {
-        async {
+        async(Banker.getInstance()) {
             // 准备重载
             ConfigManager.reload()
             sender.sendMessage(ConfigManager.reloaded)
